764227f6782245badb3c2598dcf5d8927c3ce4e2
Ramon Daniel Echeverri <71988303+Raymonddanials@users.noreply.github.com>
2024-08-29 15:44:45 -0500

    Comentarios

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 // (powered by FernFlower decompiler)
 // we need import class java.util.scanner to obtain information entered by the user
 import java.util.Scanner;
-
+//We will use java.util.scanner so that it can receive inputs
 public class Main {
     public Main() {
     }
@@ -14,6 +14,8 @@ public class Main {
                 "This program shows if the number is a multiple of 100");
         System.out.println("Enter number");
         int num = in.nextInt();
+        //We use a conditional to know if it is a multiple of 100, the % operator is used to know the remaining value of a division if it is equal ==0 it is a multiple of 100
+        /*If it is not a multiple of 100, it will give another result*/
         if (num % 100 == 0) {
             System.out.println("The number " + num + " is a multiple of 100");
         } else {
